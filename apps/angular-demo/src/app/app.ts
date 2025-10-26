3d1b78ae0af317a44b6c42c8003390e739eb9420
graqu <jacek_135@yahoo.pl>
2025-10-26 13:51:30 +0100

    add hero component

--- apps/angular-demo/src/app/app.ts
+++ apps/angular-demo/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Hero } from '@angular-demo/ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, Hero],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
